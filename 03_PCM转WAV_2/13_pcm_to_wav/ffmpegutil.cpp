13bda0e82294679c067d9f1cb28c6b99b6794906
TpTang <83406050+TpTang@users.noreply.github.com>
2021-06-15 21:57:55 +0800

    Add files via upload

---
+++ 03_PCM转WAV_2/13_pcm_to_wav/ffmpegutil.cpp
@@ -0,0 +1,44 @@
+#include "ffmpegutil.h"
+
+FFmpegUtil::FFmpegUtil(){
+
+}
+
+void FFmpegUtil::pcm2wav(WAVHeaders &header,
+                         const char *pcmFileName,
+                         const char *wavFileName){
+    header.blockAlign = header.bitsPerSample * header.channels >> 3;
+    header.byteRate = header.blockAlign * header.sampleRate;
+
+    //打开PCM文件
+    QFile pcmFile(pcmFileName);
+    if(!pcmFile.open(QFile::ReadOnly)){
+        qDebug() << "文件打开失败" << pcmFileName;
+        return;
+    }
+    header.dataChunkDataSize = pcmFile.size();
+    header.riffChunkDataSize = header.dataChunkDataSize + sizeof(WAVHeaders) - 8;
+
+    //打开WAV文件
+    QFile wavFile(wavFileName);
+    if(!wavFile.open(QFile::WriteOnly)){
+        qDebug() << "文件打开失败" << wavFileName;
+        pcmFile.close();
+        return;
+    }
+
+    //写入文件头
+    wavFile.write((const char*)&header, sizeof(WAVHeaders)); //将结构体对象写入文件头
+
+    //写入PCM数据
+    char buf[1024];
+    int size;
+    while((size = pcmFile.read(buf, sizeof(buf))) > 0){
+        wavFile.write(buf, size);
+    }
+
+    //关闭文件
+    pcmFile.close();
+    wavFile.close();
+
+}
